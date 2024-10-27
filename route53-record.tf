2742ec519c2fd2da2c2a37e38b8cd290486c0eef
davidasam141 <davidasam141@gmail.com>
2024-10-27 08:05:10 -0500

    fixed record entry

--- route53-record.tf
+++ route53-record.tf
@@ -10,7 +10,7 @@
 # }
 
 # Create an A record in the hosted zone
-resource "aws_route53_record" "2waveyyy" {
+resource "aws_route53_record" "waveyyy" {
   zone_id    = "Z00539352S8OFLGNL4UXW"
   name       = var.fqdn
   type       = "A"
